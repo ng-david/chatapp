d9f875633bfe63a29ca444b365796094151bdb2f
David Ng <davidn6597@gmail.com>
2017-08-13 10:17:54 -0400

    optimized webapp for ios

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,15 +1,7 @@
 // Main
 $(function() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
-    });
+  if (window.navigator.standalone) {
+    console.log("FJSKEMFEKS:FNMSEJKFS SUCCESSSSS");
   }
 
   let socket, isLoggedIn, id, lastMsgId;
